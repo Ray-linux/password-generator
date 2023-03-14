1fff974d3bdcc463b0edd2a0090504f66312b0cb
Ray-linux <ranirava52@gmail.com>
2023-03-14 21:43:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {RxCopy} from 'react-icons/rx'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+      <form>
+        <h2>Strong password generator</h2>
+        <div id="password">
+        <input type="text" />
+        <button><RxCopy/></button>
+        </div>
+        <h2>Customize your password</h2>
+        <div id="customize">
+              <label htmlFor="number">Number</label>
+              <input type="checkbox" name="number" id='number'/>
+              <label htmlFor="upperCase">UpperCase</label>
+              <input type="checkbox" name="uppercase" id='uppercase'/>
+              <label htmlFor="lowerCase">LowerCase</label>
+              <input type="checkbox" name="lowerCase" id='lowerCase'/>
+              <label htmlFor="symbols">Symbols</label>
+              <input type="checkbox" name="symbols" id='symbols'/>
+        </div>
+      </form>
     </div>
+    </>
   );
 }
 
