215a071b9929244ddaf1e684805ea96df9bf038c
Ray-linux <ranirava52@gmail.com>
2023-04-03 20:36:18 +0530

    adding CSS --Beta version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import {RxCopy} from 'react-icons/rx'
+import {RiRestartLine} from 'react-icons/ri'
 
 function App() {
   return (
@@ -11,18 +12,24 @@ function App() {
         <div id="password">
         <input type="text" />
         <button><RxCopy/></button>
+        <button><RiRestartLine/></button>
         </div>
         <h2>Customize your password</h2>
         <div id="customize">
+              <input type="checkbox" name="number" id='number' className="big"/>
               <label htmlFor="number">Number</label>
-              <input type="checkbox" name="number" id='number'/>
+              <input type="checkbox" name="uppercase" id='uppercase' className="big"/>
               <label htmlFor="upperCase">UpperCase</label>
-              <input type="checkbox" name="uppercase" id='uppercase'/>
+              <input type="checkbox" name="lowerCase" id='lowerCase' className="big"/>
               <label htmlFor="lowerCase">LowerCase</label>
-              <input type="checkbox" name="lowerCase" id='lowerCase'/>
+              <input type="checkbox" name="symbols" id='symbols' className="big"/>
               <label htmlFor="symbols">Symbols</label>
-              <input type="checkbox" name="symbols" id='symbols'/>
         </div>
+        <div id="passSize">
+          <input type="number"/>
+          <input type="range" name='size' min="0" max = "100"/>
+        </div>
+        <button>Copy Password</button>
       </form>
     </div>
     </>
